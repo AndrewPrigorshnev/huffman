63f893965a95b6a59b110a4f4f30602f1f5eb022
Andrew Prigorshnev <a.prigorshnev@gmail.com>
2020-12-11 22:00:23 +0400

    - C# - Move formatting of commands into the class Resources

---
+++ c-sharp/Huffman.Cli/Command.cs
@@ -0,0 +1,9 @@
+namespace Huffman.Cli
+{
+    internal sealed class Command
+    {
+        public string? Name { get; set; }
+        public string? Syntax { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- c-sharp/Huffman.Cli/Commands.cs
+++ c-sharp/Huffman.Cli/Commands.cs
@@ -2,34 +2,25 @@ namespace Huffman.Cli
 {
     internal static class Commands
     {
-        public static Command Compress = new Command
+        public static readonly Command Compress = new Command
         {
             Name = "compress",
             Syntax = "compress <source> <target>",
             Description = "Compress a 'source' file into an archive with the name 'target'",
         };
 
-        public static Command Expand = new Command
+        public static readonly Command Expand = new Command
         {
             Name = "expand",
             Syntax = "expand <source> <target>",
             Description = "Expand an archive 'source' into a file with the name 'target'",
         };
 
-        public static Command Help = new Command
+        public static readonly Command Help = new Command
         {
             Name = "help",
             Syntax = "help",
             Description = "Show help"
         };
-
-        public sealed class Command
-        {
-            public string? Name { get; set; }
-            public string? Syntax { get; set; }
-            public string? Description { get; set; }
-
-            public override string ToString() => $"{Syntax,-30}{Description}";
-        }
     }
 }
