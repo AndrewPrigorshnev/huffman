e078e581c77ea8d9829354890b0f752f36a725d9
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-11-23 00:52:46 +0400

    Implement commands

--- f-sharp/Huffman/Program.fs
+++ f-sharp/Huffman/Program.fs
@@ -1,4 +1,21 @@
-﻿[<EntryPoint>]
+﻿let compress (args: string[]) =
+    $"The file {args[0]} is compressed to {args[1]}"
+
+let expand (args: string[]) =
+    $"The file {args[0]} is expanded to {args[1]}"
+    
+let unknownCommand _ =
+    "Unknown command"
+    
+let run command =
+    match command with
+    | "compress" -> compress
+    | "expand" -> expand
+    | _ -> unknownCommand
+
+[<EntryPoint>]
 let main args =
-    printfn "Hello from Huffman"
+    run args[0] args[1..]
+    |> printfn "%s"
+
     0
