69e9f96c3f7f88c5c7851888eec4bbfd232daf9d
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-11-23 02:34:09 +0400

    Reformat

--- f-sharp/Huffman/Program.fs
+++ f-sharp/Huffman/Program.fs
@@ -1,17 +1,17 @@
 ﻿let compress source destination =
     $"The file {source} is compressed to {destination}"
-    
+
 let expand source destination =
-     $"The file {source} is expanded to {destination}"
-    
+    $"The file {source} is expanded to {destination}"
+
 let commands =
-    [
-        "compress", compress
-        "expand", expand
-    ] |> dict
+    [ "compress", compress
+      "expand", expand ]
+    |> dict
 
 [<EntryPoint>]
 let main args =
-    commands[args[0]] args[1] args[2] |> printfn "%s"
+    commands[args[0]] args[1] args[2]
+    |> printfn "%s"
+
     0
-    
